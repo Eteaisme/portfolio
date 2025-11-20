3a1e19c947789b148472177301d8e0bbbba419e4
Elliot <eteaisme@gmail.com>
2025-11-20 10:55:54 -0500

    initial portfolio

--- bank-book/portfolio/main.cpp
+++ bank-book/portfolio/main.cpp
@@ -1,5 +1,6 @@
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/dom/deprecated.hpp>
 #include <ftxui/dom/elements.hpp>
 
 using namespace ftxui;
@@ -7,52 +8,96 @@ using namespace ftxui;
 int main() {
     auto screen = ScreenInteractive::Fullscreen();
 
-    Element nav_bar = 
-        hbox({
-            text("elliot tam") | bold,
-            text(" | "),
-            text("h") | bold, text(" home"),
-            text(" | "),
-            text("p") | bold, text(" projects"),
-            text(" | "),
-            text("m") | bold, text(" mail"),
-        }) | center;
+    std::string current_screen = "home";
 
+    auto content = [&]() -> Element {
+        if (current_screen == "home") {
+            return vbox({
+                    text("hi, i'm elliot, a computing student @ the university of guelph."),
+                    text("i enjoy coffee, reading, music, math & computers."),
+                    text("looking for S25 work."),
+                    });
+        }
+        if (current_screen == "projects") {
+            return text("projects go here") | center | bold;
+        } 
+        if (current_screen == "mail") {
+            return vbox({
+                    text("contact me at: "),
+                    text("eteaisme@gmail.com ") | bold
+                    }) | center;
+        }
+        return text("");
+    };
 
-    Element footer =
-        hbox({
-            text("made with "),
-            text("") | bold,          
-            text(" in "),
-            text("🇨🇦"),
-        }) | center;
+    auto document = Renderer([&] {
 
-    Element inner_box =
-        vbox({
-            nav_bar,
-            filler(),
-            separator(),
-            footer,
-            separator(),
-        }) 
-        | size(HEIGHT, GREATER_THAN, 10)
-        | size(WIDTH, GREATER_THAN, 40)
-        | center;
+        Element nav_bar =
+            hbox({
+                text("    elliot tam    ") | bold,
+                text("    h") | bold, 
+                (current_screen == "home" ? text(" home    ") | bold 
+                                          : text(" home    ")),
+                text("    p") | bold, 
+                (current_screen == "projects" ? text(" projects    ") | bold
+                                              : text(" projects    ")),
+                text("    m") | bold, 
+                (current_screen == "mail" ? text(" mail    ") | bold
+                                          : text(" mail    ")),
+            }) | center | border;
 
-    Element outer_box =
-        vbox({
+        Element footer =
+            hbox({
+                text("made with "),
+                text("") | bold,
+                text(" in "),
+                text("🇨🇦"),
+                text("    |    q") | bold,
+                text(" quit"),
+            }) | center;
+
+        Element inner_box =
+            vbox({
+                nav_bar,
+                filler(),
+                content(),
+                filler(),
+                footer,
+            }) 
+            | size(HEIGHT, GREATER_THAN, 40)
+            | size(WIDTH, GREATER_THAN, 80)
+            | center;
+
+        return vbox({
             filler(),
             hbox({
                 filler(),
-                inner_box | borderLight,   
+                inner_box,
                 filler(),
             }),
             filler(),
         });
-
-    auto document = Renderer([&] {
-        return outer_box;
     });
 
-    screen.Loop(document);
+    auto main_component = CatchEvent(document, [&](Event event) {
+        if (event == Event::Character('h')) {
+            current_screen = "home";
+            return true;
+        }
+        if (event == Event::Character('p')) {
+            current_screen = "projects";
+            return true;
+        }
+        if (event == Event::Character('m')) {
+            current_screen = "mail";
+            return true;
+        }
+        if (event == Event::Character('q')) {
+            screen.Exit();
+            return true;
+        }
+        return false;
+    });
+
+    screen.Loop(main_component);
 }
